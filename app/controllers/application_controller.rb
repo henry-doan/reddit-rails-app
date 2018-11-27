2ed3f5cd89499530c8817499ba83e5ce5e56a6e1
Henry Doan <henrydoan96@gmail.com>
2018-11-26 20:02:29 -0700

    finished subs controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # protect_from_forgery with: :exception
+  # before_action :verify_paid
+
+  def verify_paid
+    @paid = true
+  end
+
+  # user login
+
 end
